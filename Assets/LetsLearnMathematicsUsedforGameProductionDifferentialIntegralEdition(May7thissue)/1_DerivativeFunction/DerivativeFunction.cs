5d6b4c36f0b628127baa103641c239de166e239f
yu-zyu <siragaigarasi@gmail.com>
2021-06-14 23:34:18 +0900

    導関数をUnityで再現した

---
+++ Assets/LetsLearnMathematicsUsedforGameProductionDifferentialIntegralEdition(May7thissue)/1_DerivativeFunction/DerivativeFunction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DerivativeFunction : MonoBehaviour
+{
+    public float h;
+
+    public float func(float x)
+    {
+        return x * x;
+    }
+    public float derivative(float x)
+    {
+        return (func(x + h) - func(x)) / h;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = func(3f);
+        Debug.Log(y);
+        float y1 = derivative(3f);
+        Debug.Log(y1);
+    }
+}
