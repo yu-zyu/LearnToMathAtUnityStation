3f67b355e5a4475118e4d669521514b1045af058
yu-zyu <siragaigarasi@gmail.com>
2021-06-21 03:31:35 +0900

    積分で円周率を出してみる　をUnityで再現した

---
+++ Assets/LetsLearnMathematicsUsedforGameProductionDifferentialIntegralEdition(May7thissue)/2_Integral/Integral.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Integral : MonoBehaviour
+{
+    float func(float x)
+    {
+        return 4f / (1f + x * x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float r = 0f;
+        const float dx = 0.1f;
+        for(var x = 0f; x < 1f; x += dx)
+        {
+            r += func(x) * dx;
+        }
+        Debug.Log(r);
+    }
+}
