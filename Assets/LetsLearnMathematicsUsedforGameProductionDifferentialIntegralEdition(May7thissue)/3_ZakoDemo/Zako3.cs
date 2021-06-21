6ed236adfa6c4288c059b009d615d8927ccffd04
yu-zyu <siragaigarasi@gmail.com>
2021-06-21 21:07:08 +0900

    雑魚敵３を作った

---
+++ Assets/LetsLearnMathematicsUsedforGameProductionDifferentialIntegralEdition(May7thissue)/3_ZakoDemo/Zako3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zako3 : MonoBehaviour
+{
+    Vector3 p1;
+    const float LEN = 10f;
+
+    private void Start()
+    {
+        p1.x = LEN;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        p1.x += -pos.x  * Time.deltaTime;
+        pos.x += p1.x * Time.deltaTime;
+        transform.position = pos;
+    }
+}
+
