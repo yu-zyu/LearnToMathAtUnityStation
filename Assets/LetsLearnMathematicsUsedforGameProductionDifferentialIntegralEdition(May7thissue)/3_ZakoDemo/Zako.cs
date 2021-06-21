4e816fa4fea7e66deee777dd4c83e0e977b3fd35
yu-zyu <siragaigarasi@gmail.com>
2021-06-21 20:35:42 +0900

    雑魚敵１作った

---
+++ Assets/LetsLearnMathematicsUsedforGameProductionDifferentialIntegralEdition(May7thissue)/3_ZakoDemo/Zako.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zako : MonoBehaviour
+{
+    const float LEN = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        pos.x = Mathf.Sin(Time.time) * LEN;
+        transform.position = pos;
+        
+    }
+}
---
+++ Assets/LetsLearnMathematicsUsedforGameProductionDifferentialIntegralEdition(May7thissue)/3_ZakoDemo/Zako1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zako1 : MonoBehaviour
+{
+    const float LEN = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        pos.x += Mathf.Cos(Time.time) * LEN * Time.deltaTime;
+        transform.position = pos;
+    }
+}
