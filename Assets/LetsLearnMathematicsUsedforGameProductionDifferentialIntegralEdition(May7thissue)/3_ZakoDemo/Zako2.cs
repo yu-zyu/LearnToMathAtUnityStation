4cf4d453bd76f27727b8b83ca2d1f0b8c8c84f8c
yu-zyu <siragaigarasi@gmail.com>
2021-06-21 20:48:49 +0900

    雑魚敵２を作った

---
+++ Assets/LetsLearnMathematicsUsedforGameProductionDifferentialIntegralEdition(May7thissue)/3_ZakoDemo/Zako2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zako2 : MonoBehaviour
+{
+    Vector3 p1;
+    const float LEN = 10f;
+
+    private void Start()
+    {
+        p1.x = LEN;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        p1.x += -Mathf.Sin(Time.time) * LEN * Time.deltaTime;
+        pos.x += p1.x * Time.deltaTime;
+        transform.position = pos;
+    }
+}
