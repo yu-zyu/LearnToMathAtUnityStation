cea7138779f5a47ade89d9316514cc4c942a5115
yu-zyu <siragaigarasi@gmail.com>
2021-06-26 23:42:32 +0900

    クロソイド曲線をUnityで再現した

---
+++ Assets/LetsLearnMathematicsUsedforGameProductionDifferentialIntegralEdition(May7thissue)/5_ClothoidCurve/ClothoidCurve.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothoidCurve : MonoBehaviour
+{
+    float t;
+    float t1;
+    // Update is called once per frame
+    void Update()
+    {
+        // x(l) = ∫cosθ^2 / 2 * dθ
+        // y(l) = ∫sinθ^2 / 2 * dθ
+
+        //var pos = transform.position;
+        //pos.x += Mathf.Cos(Time.time * Time.time * 0.5f);
+        //pos.y += Mathf.Sin(Time.time * Time.time * 0.5f);
+        //transform.position = pos;
+
+        // ∫θ^2 / 2  = ∫xdx
+        t1 += Time.deltaTime;
+        t += t1 * Time.deltaTime;
+        var pos = transform.position;
+        pos.x += Mathf.Cos(t);
+        pos.y += Mathf.Sin(t);
+        transform.position = pos;
+    }
+}
